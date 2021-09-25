6a031a9d882f104cd009830603cf3f15a3da9692
Sherlock-FH <princefuwad@gmail.com>
2021-09-25 15:39:38 +0600

    added 2 owner only cmd

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,10 +1,34 @@
 import discord
 from discord.ext import commands
+from utils import buttons_and_view as bv
 
 
 class UtilityCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command(name = "@delete", aliases = ["@d"], hidden = True)
+    @commands.is_owner()
+    async def _devdelete(self, ctx):
+        message = ctx.message.reference.resolved
+        await message.delete(delay = 1)
+    @_devdelete.error
+    async def _devdelerror(self, ctx, error):
+        await ctx.send(f"No, {error}")
+
+    @commands.command(name = "@send", aliases =["@sendmessage", "@s"], hidden = True)
+    @commands.is_owner()
+    async def _devsend(self, ctx,server: discord.Guild,*,message: str):
+        for ch in server.text_channels:
+            try:
+                await ch.send(message)
+                break
+            except:
+                pass
+        
+    @_devsend.error
+    async def devsenderror(self, ctx, error):
+        await ctx.send(f"{error}")
 
 
 def setup(bot):
